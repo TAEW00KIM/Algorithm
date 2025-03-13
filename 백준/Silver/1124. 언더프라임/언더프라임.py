8cda7f8904c37c85897c79381e17fb86819bd8e0
TAEW00KIM <90167750+TAEW00KIM@users.noreply.github.com>
2025-03-13 11:59:42 +0900

    [Silver I] Title: 언더프라임, Time: 1356 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1124. 언더프라임/언더프라임.py
@@ -0,0 +1,29 @@
+import math
+
+def solution(n):
+    arr = []
+    x = 2
+    while n > 1:
+        if n % x == 0:
+            arr.append(x)
+            n = n // x
+        else:
+            x += 1
+            if x > int(n ** 0.5): x = n
+    return arr
+
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0: return False
+    return True
+
+a, b = map(int, input().split())
+cnt = 0
+for i in range(a, b + 1):
+    result = solution(i)
+    if len(result) == 1: continue
+    if is_prime(len(result)) == True: cnt += 1
+    else: continue
+
+print(cnt)
+        
