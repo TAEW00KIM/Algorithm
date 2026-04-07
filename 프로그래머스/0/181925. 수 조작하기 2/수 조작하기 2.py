6dfd4c91e2db67d81cda0ae4ef0ddc2de05efaaf
Taewoo Kim <90167750+TAEW00KIM@users.noreply.github.com>
2026-04-07 11:24:58 +0900

    [level 0] Title: 수 조작하기 2, Time: 23.64 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,12 +1,4 @@
 def solution(numLog):
-    ans = ''
-    for i in range(len(numLog) - 1):
-        if numLog[i + 1] == numLog[i] + 1:
-            ans += 'w'
-        elif numLog[i + 1] == numLog[i] - 1:
-            ans += 's'
-        elif numLog[i + 1] == numLog[i] + 10:
-            ans += 'd'
-        else:
-            ans += 'a'
-    return ans
+    return ''.join({
+        1: 'w', -1: 's', 10: 'd', -10: 'a'
+    }[b - a] for a, b in zip(numLog, numLog[1:]))
